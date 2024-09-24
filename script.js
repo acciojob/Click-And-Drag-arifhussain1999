e4301fb168220dcc8c4d15879be5ed3116873d5f
Arif Hussain <arifh9821@gmail.com>
2024-09-24 14:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Your code here.
+let isDown=false;
+let startX;
+let scrollLeft;
+const container=document.querySelector('.items');
+
+container.addEventListener('mousedown',(e)=>{
+	e.preventDefault(); // Prevent default behavior like text selection
+  isDown = true;      // Set the flag to true to indicate dragging
+  startX = e.pageX;   // Store the initial x position
+});
+
+container.addEventListener('mousemove',(e)=>{
+	if (!isDown) return; // return if not dragging
+	const distance = e.pageX - startX;
+  container.scrollLeft -= distance; // Scroll the container
+  startX = e.pageX; // Update startX for the next movement
+});
+
+container.addEventListener('mouseup',(e)=>{
+	isDown=false;
+});
+
+container.addEventListener('mouseleave',()={
+	isDown=false;
+});
